ed271d483df2b33fc28435387150b70ad41e422e
hardikk2002 <hardikk2002@gmail.com>
2021-11-18 23:52:46 +0530

    db setup

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# to make schema ary has the follo
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # when we want to view the object then what it should show
+    def __repr__(self) -> str:
+        return f"{self.sno}- {self.title}"
 
 @app.route("/")
 def hello_world():
